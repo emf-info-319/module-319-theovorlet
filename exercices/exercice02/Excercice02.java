b128be937146855434b8621a96155d1f7c9f0f9b
theovorlet <theo.vorlet@studentfr.ch>
2024-08-29 11:46:12 +0200

    Exercice03

---
+++ exercices/exercice02/Excercice02.java
@@ -0,0 +1,34 @@
+public class Excercice02 { 
+   
+    public static void main(String[] args) {
+        
+        System.out.println("bonjour tout le monde");
+
+        char lettre = 'B';
+        System.out.println(lettre);
+
+        byte myNumOne = 127;
+        System.out.println(myNumOne);
+
+        if (myNumOne == 20) {
+
+            System.out.println("Hello");
+           
+            
+        }
+
+
+
+
+
+    
+        
+     
+     
+     
+        
+    }
+   
+
+   
+}
