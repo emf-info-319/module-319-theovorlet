6d7d8bbd109f54a41c218080bdc79b09e9c243d3
theovorlet <theo.vorlet@studentfr.ch>
2024-09-12 10:27:46 +0200

    excercice 9 génération du chiffre rdm

--- exercices/exercice09/tableauDeValeursAleatoire.java
+++ exercices/exercice09/tableauDeValeursAleatoire.java
@@ -1,8 +1,27 @@
 package exercices.exercice09;
 
+import java.util.Random;
+
 public class tableauDeValeursAleatoire {
     public static void main(String[] args) {
+
+        final int nbrMax = 6;
+         int [] tableauAleatoire = new int[10] ;  
+
+        Random chiffrePasPlusGrand6 = new Random();
+        int leChiffre = chiffrePasPlusGrand6.nextInt(nbrMax);
+        System.out.println(leChiffre);
+
+        tableauAleatoire[0] = leChiffre ;
         
+
     }
     
 }
+
+
+
+
+ 
+        
+
