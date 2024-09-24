467aee57741f07fadc22698e1db5c2f30513a628
theo <theo.vorlet@studentfr.ch>
2024-09-24 18:51:05 +0200

    devoir03

---
+++ devoirs/devoir03/devoir03.java
@@ -0,0 +1,35 @@
+public class devoir03 {
+    public static void main(String[] args) 
+    {
+        int jour;
+        int mois;
+        int annee;
+        int nbJour = 0;
+        
+        jour = 31;
+        mois = 12;
+        annee = 2024;
+        nbJour += jour;
+
+        for (int i = 1; i < mois; i++)
+        {
+            if (i == 2 && (annee % 4) == 0)
+            {
+                nbJour += 29;
+            }
+            else if (i == 2 && (annee % 4) != 0)
+            {
+                nbJour += 28;
+            }
+            else if ((i % 2) != 0)
+            {
+                nbJour += 31;
+            }
+            else if ((i % 2) == 0)
+            {
+                nbJour += 30;
+            }
+        }
+        System.out.println("nous sommes le jours : " + nbJour);
+    }
+}
