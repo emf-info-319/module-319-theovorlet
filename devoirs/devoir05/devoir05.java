c3b09897986463e69ea39dfe3c4d7ba0636f5d23
theo <theo.vorlet@studentfr.ch>
2024-10-08 17:23:05 +0200

    devoir 05

--- devoirs/devoir05/devoir05.java
+++ devoirs/devoir05/devoir05.java
@@ -1,11 +1,30 @@
 package devoirs.devoir05;
 
+import java.util.Scanner;
+
 public class devoir05 {
     public final static int NOMBRE_MAX = 100;
     public final static int NOMBRE_MIN = 0;
 
     public static void main(String[] args) {
         int nombreADeviner = (int)( Math.random() * ( NOMBRE_MAX- NOMBRE_MIN + 1 )) + NOMBRE_MIN;
-        
+        Scanner demandeChiffre= new Scanner(System.in);
+        boolean chiffreTrouver= false;
+        while (chiffreTrouver==false) {
+            System.out.println("Entrez une valeur svp : ");
+            int valeurDonner = demandeChiffre.nextInt();
+            demandeChiffre.nextLine();
+            if (valeurDonner == nombreADeviner) {
+                System.out.println("Bravo vous avez trouver");
+                chiffreTrouver= true;
+            }else if (valeurDonner<nombreADeviner) {
+                System.out.println("Trop petit");
+            }else if (valeurDonner >nombreADeviner) {
+                System.out.println("Trop grand");
+            }
+            
+
+        }
+        demandeChiffre.close();
     }
 }
