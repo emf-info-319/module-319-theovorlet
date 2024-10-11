0be136e7b48d8923b31e50af595194418816b09e
theovorlet <theo.vorlet@studentfr.ch>
2024-10-11 10:17:25 +0200

    binaire

---
+++ 162/binaire.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public int calcul(){
+        
+}
+public class binaire {
+  
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Veuillez entrer un nombre décimal : ");
+        int nombreDecimal = scanner.nextInt();
+        scanner.close();
+    }
+}
