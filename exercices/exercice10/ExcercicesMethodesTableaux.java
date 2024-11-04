fbaff15133e843ee134c803a8a95ed2ead639edf
theo <theo.vorlet@studentfr.ch>
2024-11-04 18:51:21 +0100

    excercice 10

--- exercices/exercice10/ExcercicesMethodesTableaux.java
+++ exercices/exercice10/ExcercicesMethodesTableaux.java
@@ -3,16 +3,32 @@ package exercices.exercice10;
 public class ExcercicesMethodesTableaux {
     public static int MAX = 10;
     public static int MIN = 1;
+    public static int TAILLEDUTABLEAU = 5;
+    public static int VALEURVOULU = 2;
+    public static int VALEURATROUVER = 2;
+    public static int VALEURACHERCHER = 5;
+    public static int VALEURQUIREMPLACE = 4;
+    public static int VALEURATROUVERPREMIEREMPLACEMENT = 3;
+    public static int VALEURATROUVERDERNIEREMPLACEMENT = 2;
 
     public static void main(String[] args) {
-        int[] monTableau = creationTableaux(5);
-        int[] monTableauRemplieFixe = remplissageFixeDuTableau(monTableau, 2);
+
+        int[] monTableau = creationTableaux(TAILLEDUTABLEAU);
+        int[] monTableauRemplieFixe = remplissageFixeDuTableau(monTableau, VALEURVOULU);
         int[] monTableauRemplieAleatoir = remplissageAleatoirDuTableau(monTableau);
         int maTailleDuTableauTrouver = DeterminationDeLaTailleTableau(monTableau);
         afficherContenuTableau(monTableau);
         int valeurMinDuTableau = valeurMinTableau(monTableauRemplieAleatoir);
         int valeurMaxDuTableau = valeurMaxTableau(monTableauRemplieAleatoir);
-        int nombreDAppartionDUneValeur = nbrDApparitionDUneValeur(monTableauRemplieAleatoir, 2);
+        int nombreDAppartionDUneValeur = nbrDApparitionDUneValeur(monTableauRemplieAleatoir, VALEURATROUVER);
+       
+        int[] remplacementDeValeur = remplacementValeurTableau(monTableau, VALEURACHERCHER, VALEURQUIREMPLACE);
+        System.out.println("Après remplacement de la valeur " + VALEURACHERCHER + " par la valeur " + VALEURQUIREMPLACE + " voici le contenu du tableau : ");
+        afficherContenuTableau(monTableau);
+        int sommeValeurDuTableau = sommeValeurTableau(monTableau);
+        double moyenneTableau = moyenneValeurTableau(monTableau);
+        int emplacementValeurDansTableau = premierEmplacementValeur(monTableau, VALEURATROUVERPREMIEREMPLACEMENT);
+        int dernierEmplacementDuneValeur = dernierEmplacementValeur(monTableau, VALEURATROUVERDERNIEREMPLACEMENT);
     }
 
     public static int[] creationTableaux(int tailleDuTableau) {
@@ -36,12 +52,14 @@ public class ExcercicesMethodesTableaux {
 
     public static int DeterminationDeLaTailleTableau(int[] tableauTailleADefinir) {
         int tailleTableauTrouver = tableauTailleADefinir.length;
+        System.out.println("le tableau a une taille de " + tailleTableauTrouver);
         return tailleTableauTrouver;
     }
 
     public static void afficherContenuTableau(int[] tableauAAfficher) {
+        System.out.println("Contenu du tableau aléatoir : ");
         for (int k = 0; k < tableauAAfficher.length; k++) {
-            System.out.println(tableauAAfficher[k]);
+            System.out.println("tab[" + k + "]" + tableauAAfficher[k]);
         }
     }
 
@@ -52,6 +70,7 @@ public class ExcercicesMethodesTableaux {
                 minimumTrouver = minTableau[l];
             }
         }
+        System.out.println("la valeur min trouvée = " + minimumTrouver);
         return minimumTrouver;
     }
 
@@ -62,17 +81,71 @@ public class ExcercicesMethodesTableaux {
                 maximumTrouver = maxTableau[m];
             }
         }
+        System.out.println("la valeur max trouvée = " + maximumTrouver);
         return maximumTrouver;
     }
 
     public static int nbrDApparitionDUneValeur(int[] tableauAInspecter, int valeurATrouver) {
         int nbrDeFois = 0;
-        for (int n= 0; n < tableauAInspecter.length; n++) {
+        for (int n = 0; n < tableauAInspecter.length; n++) {
             if (tableauAInspecter[n] == valeurATrouver) {
                 nbrDeFois++;
             }
         }
         return nbrDeFois;
+    }
+
+    public static int sommeValeurTableau(int[] tableauAAditionner) {
+        int somme = 0;
+        for (int o = 0; o < tableauAAditionner.length; o++) {
+            somme += tableauAAditionner[o];
+        }
+        System.out.println("La somme des cellules du tableau = "+somme);
+        return somme;
+    }
+
+    public static double moyenneValeurTableau(int[] tableauAFaireMoyen) {
+        double moyenne = 0;
+        for (int p = 0; p < tableauAFaireMoyen.length; p++) {
+            moyenne += tableauAFaireMoyen[p];
+        }
+        moyenne /= tableauAFaireMoyen.length;
+        System.out.println("La moyenne des cellules du tableau = "+moyenne);
+        return moyenne;
+    }
+
+    public static int[] remplacementValeurTableau(int[] tableauAModifier, int valeurAChercher, int valeurQuiRemplace) {
+        for (int q = 0; q < tableauAModifier.length; q++) {
+            if (tableauAModifier[q] == valeurAChercher) {
+                tableauAModifier[q] = valeurQuiRemplace;
+            }
+        }
+
+        return tableauAModifier;
+    }
+
+    public static int premierEmplacementValeur(int[] tableauEmplacementValeur, int valeurEmplacement) {
+        for (int r = 0; r < tableauEmplacementValeur.length; r++) {
+            if (tableauEmplacementValeur[r] == valeurEmplacement) {
+                System.out.println("La valeur  " + valeurEmplacement +  " a été trouvée à la 1ère position N" +r);
+                return r;
+            }
+
+        }
+        System.out.println("La valeur  " + valeurEmplacement +  " a été trouvée à la 1ère position N-1");
+        return -1;
+    }
+
+    public static int dernierEmplacementValeur(int[] tableauDerniereValeur, int derniereValeur) {
+        for (int s = tableauDerniereValeur.length - 1; s >= 0; s--) {
+            if (tableauDerniereValeur[s] == derniereValeur) {
+                System.out.println("La valeur  " + derniereValeur +  " a été trouvée à la dernière position N"+ s);
+                return s;
+            }
+        }
+        System.out.println("La valeur  " + derniereValeur +  " a été trouvée à la dernière position N-1");
+        return -1;
+
     }
 
 }
